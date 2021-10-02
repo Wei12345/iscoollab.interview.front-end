094a0606a0fa426e047b6d8bed4c103dce45a697
Wei <weihsu@inquartik.com>
2021-10-03 00:51:43 +0800

    build: add babel, eslint and react

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+  },
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+    ]
+  }
 };
