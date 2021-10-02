319d74e661c91b56d61332715d3fdc8470a12aee
Wei <weihsu@inquartik.com>
2021-10-03 04:15:03 +0800

    build: add build script and production webpack

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
   entry: {
     index: './src/index.js',
   },
