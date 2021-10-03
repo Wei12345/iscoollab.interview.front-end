b006d180b84f3e60ca30ebd6bf3e37e717d1f750
Wei <weihsu@inquartik.com>
2021-10-03 19:37:48 +0800

    feat: adjust menus data

--- src/data/menus.js
+++ src/data/menus.js
@@ -1,8 +1,60 @@
-const data =[
+import uniqueId from "../lib/uniqueId";
+
+const data = [
   {
+    id: uniqueId(),
     category: 'Food',
-    items: ['foodA', 'foodB', 'foodC']
+    items: [
+      {
+        id: uniqueId(),
+        item: 'foodA',
+      },
+      {
+        id: uniqueId(),
+        item: 'foodB'
+      },
+      {
+        id: uniqueId(),
+        item: 'foodC'
+      }
+    ]
   },
+  {
+    id: uniqueId(),
+    category: 'Drink',
+    items: [
+      {
+        id: uniqueId(),
+        item: 'drinkQ',
+      },
+      {
+        id: uniqueId(),
+        item: 'drinkW',
+      },
+      {
+        id: uniqueId(),
+        item: 'drinkE',
+      }
+    ]
+  },
+  {
+    id: uniqueId(),
+    category: 'Others',
+    items: [
+      {
+        id: uniqueId(),
+        item: 'other1',
+      },
+      {
+        id: uniqueId(),
+        item: 'other2',
+      },
+      {
+        id: uniqueId(),
+        item: 'other3',
+      }
+    ]
+  }
 ]
 
 export default data;
